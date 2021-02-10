c5206326cd115dd4071a99989104374adee55aae
zenghl <986850700@qq.com>
2021-02-10 20:17:34 +0800

    增加swagger接口文档

---
+++ src/main/java/com/gy/vhronceagainserver/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.gy.vhronceagainserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @description: swagger配置类
+ * @author: zenghl
+ * @create: 2021-02-10 13:01
+ **/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.gy.vhronceagainserver.controller"))
+                .paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Kitty API Doc")
+                .description("This is a restful api document of Kitty.")
+                .version("1.0")
+                .build();
+    }
+
+}
--- src/main/java/com/gy/vhronceagainserver/controller/HelloController.java
+++ src/main/java/com/gy/vhronceagainserver/controller/HelloController.java
@@ -1,11 +1,15 @@
 package com.gy.vhronceagainserver.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "description of class")
 @RestController
 public class HelloController {
 
+    @ApiOperation(value = "description of method", notes = "")
     @GetMapping("/hello")
     public String hello(){
         return "hello, zenghl";
